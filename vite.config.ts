a4fb6a74b2cd3a17bae59944d1524c02574047c7
Claudio Silva <claudio@assetpark.cl>
2026-04-29 12:55:43 -0400

    fix: move all build deps to dependencies, remove devServer

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ const __dirname = import.meta.dirname
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react()
